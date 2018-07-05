db8e4c79dd068f574069d0ef1f6f047616b0925d
sunxiaofeng123 <18703137630@163.com>
2018-07-05 20:25:08 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,9 +2,16 @@
 <html>
 <head>
     <title>@yield('title','Sample') - Lravel5.5</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 
 <body>
-@yield('content')
+@include('layouts._header')
+
+<div class = 'container'>
+    @yield('content')
+    @include('layouts._footer')
+</div>
+
 </body>
 </html>
